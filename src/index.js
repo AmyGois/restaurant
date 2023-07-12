b5bc820b31aa35a35fccdee83927dc78bf6504a1
AmyGois <ahagois@hotmail.com>
2023-07-12 19:11:55 +0100

    Make the buttons change the tabs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import loadPage from './loadPage';
+import loadAboutTab from './loadAboutTab';
 import loadMenuTab from './loadMenuTab';
 import loadContactTab from './loadContactTab';
 
-console.log('Up and running!');
-
-/* Make this reusable later */
+/* Update buttons display */
 const updateButtons = (buttonClass) => {
   const buttons = document.querySelectorAll('.button-navigation');
   buttons.forEach((button) => {
@@ -16,6 +15,43 @@ const updateButtons = (buttonClass) => {
   });
 };
 
-loadContactTab();
+updateButtons('button-tab-1');
 
-updateButtons('button-tab-3');
+/* Remove the content that is going to be changed */
+const removeContent = () => {
+  const mainContent = document.querySelector('.main-content');
+  mainContent.innerHTML = '';
+};
+
+const displayAboutTab = () => {
+  removeContent();
+  updateButtons('button-tab-1');
+  loadAboutTab();
+};
+
+const displayMenuTab = () => {
+  removeContent();
+  updateButtons('button-tab-2');
+  loadMenuTab();
+};
+
+const displayContactTab = () => {
+  removeContent();
+  updateButtons('button-tab-3');
+  loadContactTab();
+};
+
+const addButtonListeners = (() => {
+  const buttons = document.querySelectorAll('.button-navigation');
+  buttons.forEach((button) => {
+    if (button.classList.contains('button-tab-1')) {
+      button.addEventListener('click', displayAboutTab);
+    }
+    if (button.classList.contains('button-tab-2')) {
+      button.addEventListener('click', displayMenuTab);
+    }
+    if (button.classList.contains('button-tab-3')) {
+      button.addEventListener('click', displayContactTab);
+    }
+  });
+})();
